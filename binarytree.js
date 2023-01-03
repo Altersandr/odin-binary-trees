6db21dfa1ffe4230cbd0e3851c74a9912bed2c3e
Oleks <archer.opendub@gmail.com>
2023-01-03 14:10:30 +0100

    added the find, levelorder, preorder, inorder, postorder methods to the tree

--- binarytree.js
+++ binarytree.js
@@ -53,6 +53,7 @@ class Tree {
       let current = this.root;
       let prev = null;
       let side;
+      let toRemove;
       //i go inside the loop, my current is my root, check if the root value is
       //bigger or smaller, if equal, set my current to null, 
       //if smaller go left, else go right.
@@ -68,14 +69,79 @@ class Tree {
           prev = current;
           current = current.right;
           side = "right"
+        }else{
+
+          //to delete a node with 2 children you first go to the right of the node
+          //then recursively go to left to find the next just bigger node
+          //once you find it make that node the new parent of the children
+
+          if(value==current.data&&current.left!==null) return prev.left = current.left;
+          if(value==current.data&&current.right!==null) return prev.right = current.right;
+          if(value==current.data && side =="left") return prev.left = null;
+          else if(value==current.data && side =="right") return prev.right = null;
         }
-        if(value==current.data && side =="left") return prev.left = null;
-        else if(value==current.data && side =="right") return prev.right = null;
       } 
     }
-      
-       
 
+    find(value, current = this.root){
+        if(current.data ==value) return current
+        else if(value<current.data) return this.find(value, current.left)
+        else return this.find(value, current.right)
+    }
+
+    levelOrder(root){
+      if(root==null)return console.log(array)
+      let array = []
+      let queue = [];
+      queue.push(root);
+      while(queue[0]!==undefined){
+        let current = queue[0];
+        array.push(current.data)
+        if(current.left!==null)queue.push(current.left);
+        if(current.right!==null)queue.push(current.right);
+        queue.shift()
+      }
+
+      return console.log(array)
+    }
+
+    preorder(root, result = []){
+      if(root==null) return
+      result.push(root.data)
+      this.preorder(root.left, result)
+      this.preorder(root.right, result)
+      return result
+    }
+
+    inorder(root, result = []){
+      if(root==null)return 
+      this.preorder(root.left, result)
+      result.push(root.data)
+      this.preorder(root.right, result)
+      return result
+    }
+
+    postorder(root, result = []){
+      if(root==null)return 
+      this.preorder(root.left, result)
+      this.preorder(root.right, result)
+      result.push(root.data)
+      return result
+    }
+
+    height(node, current = this.root){
+      let height = 0;
+      console.log(current)
+      if(current.data ==node) return height
+      else if(node<current.data) {
+        height+=1
+        return this.find(node, current.left)}
+      else 
+      { height+=1
+        return this.find(node, current.right)}
+    }
+  
+            
 };
 
 const ar =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
@@ -108,10 +174,25 @@ binaryTree.insert(340);
 binaryTree.insert(25);
 binaryTree.insert(2);
 binaryTree.insert(100);
+binaryTree.insert(99);
+binaryTree.insert(98);
+binaryTree.insert(97);
+binaryTree.insert(96);
+binaryTree.insert(200);
 binaryTree.insert(7754);
 prettyPrint(binaryTree.root)
-binaryTree.delete(25);
-binaryTree.delete(1);
+binaryTree.delete(23);
+// binaryTree.delete(1);
+binaryTree.delete(98);
+binaryTree.delete(200);
+
+binaryTree.preorder(binaryTree.root)
+
+binaryTree.inorder(binaryTree.root)
+
+// binaryTree.levelOrder(binaryTree.root)
 prettyPrint(binaryTree.root)
+
+
 
 
